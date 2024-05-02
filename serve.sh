11a64146f43682cda120d55bbc45fa1f75ffb55a
Peter van Heusden <pvanheusden@uwc.ac.za>
2024-05-02 20:03:58 +0200

    Add background material

---
+++ serve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d .bundler ] ; then mkdir .bundler ; fi
+ 
+docker run -i -t --rm -u $(id -u):$(id -g) \
+    -p 4000:4000 -v $(pwd):/opt/app \
+    -v $(pwd)/.bundler/:/opt/bundler \
+    -e BUNDLE_PATH=/opt/bundler \
+    -w /opt/app ruby:2.7 bash \
+    -c "bundle install && bundle exec jekyll serve --watch -H 0.0.0.0"
